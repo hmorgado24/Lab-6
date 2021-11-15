204479ab623d775a27420c8fb28005f50a0cb67a
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-15 21:41:51 +0000

    asd

--- smile.py
+++ smile.py
@@ -3,7 +3,7 @@ from led8x8 import LEDdisplay
 from shiftregisterclass import Shifter
 
 data, latch, clock = 21, 19, 26
-leddisp = LEDdisplay(data, latch, clock)
+leddisp = Shifter(data, latch, clock)
 
 pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 
 0b10100101, 0b10011001, 0b01000010, 0b00111100]
@@ -11,5 +11,5 @@ pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,
 while True:
   for n in range(len(pattern)):
 
-    leddisp.shiftbyte(pattern[n])
+    leddisp.Shifter.shiftbyte(pattern[n])
     time.sleep(.4)
