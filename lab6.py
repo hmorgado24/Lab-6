d39ee5173916107e9fb34934082e778e80ef4aa6
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 17:42:41 +0000

    sdf

--- lab6.py
+++ lab6.py
@@ -38,7 +38,7 @@ multi.start
 
 while True:
 
-  dataPin, latchPin, clockPin = 21, 19, 26
-  leddisp = led(led.dataPin, led.latchPin, led.clockPin)
+  data, latch, clock = 21, 19, 26
+  leddisp = led(data, latch, clock)
   
   lightningbug(data, latch, clock)
