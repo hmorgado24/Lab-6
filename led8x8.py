870eb1502b576ea5504dbaa59a579a26aa62d246
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 03:19:17 +0000

    j

--- led8x8.py
+++ led8x8.py
@@ -3,21 +3,19 @@ import time
 
 class LEDdisplay():
 
-  rows = [0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001]
-
   pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001, 0b10100101, 0b10011001, 0b01000010, 0b00111100]
 
   def __init__(self, data, latch, clock):
     self.Shifter = Shifter(data, latch, clock)
  
-  def display(self, num):  # display a given number
-    self.Shifter.shiftByte(LEDdisplay.rows[num])
-    self.Shifter.shiftByte(LEDdisplay.pattern[num])
-    
+  def display(self, row, col):  # display a given number
+    self.Shifter.shiftByte(LEDdisplay.pattern[col])
+    self.Shifter.shiftByte(LEDdisplay.pattern(1 << (row-1)))
+  
 data, latch, clock = 21, 19, 26
 pins = LEDdisplay(data, latch, clock)
 
 while True:
   for n in range(8):
-    pins.display(n)
+    pins.display(n, n)
     time.sleep(.001)
