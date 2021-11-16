4bed64b1fa1c3a56ecda02a9ad8ac482cb759815
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 02:45:49 +0000

    n

--- smile.py
+++ smile.py
@@ -2,9 +2,9 @@ import time
 from led8x8 import LEDdisplay
 
 data, latch, clock = 21, 19, 26
-leddisp = LEDdisplay(data, latch, clock)
+pins = LEDdisplay(data, latch, clock)
 
 while True:
   for n in range(8):
-    LEDdisplay.leddisp.display(n)
+    pins.LEDdisplay.display(n)
     time.sleep(.001)
