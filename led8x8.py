9ecafc0f1aeb609afd757c90baad9b700c0595f5
Hugo Morgado <91498952+hmorgado24@users.noreply.github.com>
2021-11-16 18:42:43 +0000

    ou

--- led8x8.py
+++ led8x8.py
@@ -5,8 +5,8 @@ class led():
   pat = [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000]
 
   def __init__(self, data, latch, clock):
-    self.shiftR = Shifter(data, latch, clock)
+    self.shift = Shifter(data, latch, clock)
  
   def display(self, r, c):
-    self.shiftR.shiftByte(led.pat[c])  # load the row values
-    self.shiftR.shiftByte(1 << (r-1))   # select the given row
+    self.shift.shiftByte(led.pat[c])  # load the row values
+    self.shift.shiftByte(1 << (r-1))   # select the given row
